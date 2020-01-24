55eeffd78c0f7265e4a9f326b948b930bfda7969
Renuprasad K <renu@republicka.in>
2020-01-24 18:17:22 +0530

    Updates

---
+++ precache-manifest.61bc969a8b5049df3f17e340fdd0085f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316b1e1bcd4541c4957b4d44f3f835be",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "c3f18dee77e1a556ef74",
+    "url": "/e-commerce/static/css/main.774f2412.chunk.css"
+  },
+  {
+    "revision": "1b7ccfadcf0eb55dc954",
+    "url": "/e-commerce/static/js/2.f1d392b3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/e-commerce/static/js/2.f1d392b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3f18dee77e1a556ef74",
+    "url": "/e-commerce/static/js/main.a830ab2d.chunk.js"
+  },
+  {
+    "revision": "50b65eb8b5adc10d7446",
+    "url": "/e-commerce/static/js/runtime-main.2e66108c.js"
+  }
+]);
